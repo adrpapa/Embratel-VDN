a02800c4e9b4519012613887aa4eeadbbb92d0c9
Adriano <adrpapa@gmail.com>
2016-06-24 13:58:13 -0300

    Ajustes Suporte Authentication

---
+++ workspace/VDN_Embratel/scripts/elemental_api/auth.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'configConsts.php';
+
+class Auth {
+	protected $expires;
+	protected $auth_key;
+	
+	public function __construct( $login,$key ) {
+		$this->key = $key;
+		$this->login = $login;
+	}
+
+	public function setLogin($login) {
+		$this->login = $login;
+	}
+	
+	public function setKey($key){
+		$this->key = $key;
+	}
+
+	public function getLogin() {
+		return $this->login;
+	}	
+	
+	public function getExpires() {
+		return $this->expires;
+	}
+	
+	public function getAuthKey() {
+		return $this->auth_key;
+	}
+	
+	public function createAuthKey( $url ) {
+		$this->expires=time() + 1200;
+		
+		if ( is_null($this->key) || is_null($this->login) || is_null($url) ) {
+			throw new Exception('Error:createAuthKey() Login and API Key must not be null');
+		}
+		
+		return ($this->auth_key = md5($this->key . md5($url . $this->login . $this->key . $this->expires)) );
+	}
+	
+}
+?>
